bebfce30f62fcae0c77afb34f6413c0e47546cb7
Gadi Piperno Corcos <gadi@skyriseltd.com>
2023-11-22 02:03:06 +0100

    :bug: Fixed use halley in main.rs and added pack command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
 
 use std::path::Path;
 
-use wgpack::halley;
+use halleypack::halley;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
@@ -17,9 +17,14 @@ fn unpack(src: &Path, dst: &Path, pack_version: halley::PackVersion, secret: Opt
     halley::unpack_assets(src, dst, pack_version, secret);
 }
 
+#[tauri::command(async)]
+fn pack(src: &Path, dst: &Path, pack_version: halley::PackVersion, secret: Option<&str>) {
+    halley::pack_assets(src, dst, pack_version, secret);
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![copy_assets, unpack])
+        .invoke_handler(tauri::generate_handler![copy_assets, unpack, pack,])
         .plugin(tauri_plugin_store::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
